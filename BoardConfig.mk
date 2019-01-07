4819805838a8206d53adc8a4d54d8a29689940d0
RandomPooka <jmckenna2k10@gmail.com>
2019-01-07 01:23:02 -0500

    Removed further LOS traces and proprietaries

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,8 +52,8 @@ BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_SOURCE := kernel/htc/msm8998
-TARGET_KERNEL_CONFIG := aosip_ocean_defconfig
-### TARGET_KERNEL_CONFIG := lineage_ocean_defconfig
+### TARGET_KERNEL_CONFIG := aosip_ocean_defconfig
+TARGET_KERNEL_CONFIG := lineage_ocean_defconfig
 
 BOARD_ROOT_EXTRA_FOLDERS := bt_firmware dsp firmware persist
 
@@ -203,8 +203,8 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.recovery
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
-# Lineage Hardware
- JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(DEVICE_PATH)/lineagehw|**/*.java
+### # Lineage Hardware
+###  JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(DEVICE_PATH)/lineagehw|**/*.java
 
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
--- device.mk
+++ device.mk
@@ -289,12 +289,12 @@ PRODUCT_PACKAGES += \
     lights.msm8998
 
 # LiveDisplay native
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm \
-    vendor.lineage.livedisplay-V1.0-java
+#### PRODUCT_PACKAGES += \
+####     vendor.lineage.livedisplay@1.0-service-sdm \
+####     vendor.lineage.livedisplay-V1.0-java
 
-PRODUCT_BOOT_JARS += \
-    vendor.lineage.livedisplay-V1.0-java
+#### PRODUCT_BOOT_JARS += \
+####     vendor.lineage.livedisplay-V1.0-java
 
 # Media
 PRODUCT_COPY_FILES += \
